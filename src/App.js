c2f5f476758fa766c3b32939859c5f5b02fcbaad
An Yong Shyan <yongshyan.an@gmail.com>
2024-08-21 03:12:54 +0800

    docs: updated readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1 className="text-4xl text-blue-500">Hello World</h1>
+      <h1 className="text-4xl underline font-bold">Hello World</h1>
+      <Person />
     </div>
   );
-}
+};
+
+const Person = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-blue-500">Name: John</h1>
+      <h2 className="text-3xl text-red-500">Age: 31</h2>
+    </>
+  );
+};
 
 export default App;
