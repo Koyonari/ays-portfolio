3921b3b4dbfd79815b28d20b74569309b0092e4c
An Yong Shyan <yongshyan.an@gmail.com>
2024-08-31 02:31:00 +0800

    refactor: changed about content to be in separate var

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,21 +14,20 @@ function About() {
 
       <div className="hero-container w-[60vw]" id="about">
         <h2 className="text-[8rem] leading-[8rem] mb-16 font-general-sans">
-          <span>About Me</span>
+          <span>{about.title}</span>
         </h2>
         <p className="text-[2rem] leading-loose">
-          <span>
-            As a 2nd year Information Technology student at Ngee Ann Polytechnic
-            with a long-standing passion for software and full-stack
-            development, I'm eager to learn and contribute to meaningful
-            projects. I'm seeking a full-time, year-long internship starting in
-            March 2024, where I can apply my skills and passion to the tech
-            industry.
-          </span>
+          <span>{about.description}</span>
         </p>
       </div>
     </section>
   );
 }
+
+const about = {
+  title: "About Me",
+  description:
+    "As a 2nd year Information Technology student at Ngee Ann Polytechnic with a long-standing passion for software and full-stack development, I'm eager to learn and contribute to meaningful projects. I'm seeking a full-time, year-long internship starting in March 2024, where I can apply my skills and passion to the tech industry.",
+};
 
 export default About;
