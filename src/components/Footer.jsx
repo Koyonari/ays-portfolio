f2b8116ffb6fd466c2ed28a2154b08a412da1bb2
An Yong Shyan <yongshyan.an@gmail.com>
2024-09-12 02:25:14 +0800

    style: responsive footer texts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,18 @@ import "../css/index.css";
 
 const Footer = () => {
   return (
-    <div className="footer font-general-sans mt-32 flex flex-col md:flex-row justify-between items-center w-screen px-6">
+    <div className="footer font-general-sans text-xs sm:text-sm lg:text-xl mt-32 flex flex-col md:flex-row justify-between items-center w-screen px-6">
       <p className="text-black">&copy; 2024 An Yong Shyan</p>
       <p>Site designed and developed with ❤️</p>
       <a
         href="#parallax"
-        className="scroll uppercase flex flex-row items-center cursor-pointer"
+        className="scroll flex flex-row items-center cursor-pointer"
       >
-        BACK TO TOP
-        <img src={backtotop} alt="backtotop" className="w-16 h-16 ml-2" />
+        <img
+          src={backtotop}
+          alt="backtotop"
+          className="w-5 md:w-12 mg:h-12 ml-2"
+        />
       </a>
     </div>
   );
