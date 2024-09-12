766de616a6a53dae32da1089d1a6326e640e5e24
An Yong Shyan <yongshyan.an@gmail.com>
2024-09-13 03:02:34 +0800

    style: works text responsiveness

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -12,7 +12,7 @@ function Works() {
         Recent Works.
       </h2>
 
-      <div className="works-container w-full grid gap-16 place-items-center grid-cols-[repeat(auto-fit,_minmax(600px,_1fr))]">
+      <div className="works-container w-full md:grid gap-16 place-items-center grid-cols-[repeat(auto-fit,_minmax(600px,_1fr))]">
         <div className="works-1">
           <a href="https://nutriaid-ays.vercel.app/">
             <div className="works-box1 flex justify-center items-center rounded-[20px] cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg mb-4">
@@ -25,20 +25,23 @@ function Works() {
             </div>
           </a>
 
-          <p className="flex flex-row mb-6">
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <p className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 overflow-x-auto">
               HTML<b className="font-general-sans px-3">·</b>CSS
               <b className="font-general-sans px-3">·</b>Javascript
               <b className="font-general-sans px-3">·</b>Node.js
             </p>
           </p>
-          <h3 className="link-hover text-4xl font-general-sans uppercase inline-block cursor-pointer">
-            NutriAID
-          </h3>
-          <p className="mt-3 text-3xl">Back-End Development</p>
+          <a href="https://nutriaid-ays.vercel.app/">
+            <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
+              NutriAID
+            </h3>
+          </a>
+
+          <p className="mt-3 text-xl md:text-3xl">Back-End Development</p>
         </div>
         <div className="works-2">
           <a href="https://github.com/Koyonari/ays-portfolio">
@@ -52,20 +55,22 @@ function Works() {
             </div>
           </a>
 
-          <p className="flex flex-row mb-6">
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <p className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3">
               HTML<b className="font-general-sans px-3">·</b>Javascript
               <b className="font-general-sans px-3">·</b>TailwindCSS
               <b className="font-general-sans px-3">·</b>React.js
             </p>
           </p>
-          <h3 className="link-hover text-4xl font-general-sans uppercase inline-block cursor-pointer">
-            Portfolio
-          </h3>
-          <p className="mt-3 text-3xl">Front-End Development</p>
+          <a href="https://github.com/Koyonari/ays-portfolio">
+            <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
+              Portfolio
+            </h3>
+          </a>
+          <p className="mt-3 text-xl md:text-3xl">Front-End Development</p>
         </div>
 
         <div className="works-3 w-full">
@@ -80,18 +85,20 @@ function Works() {
             </div>
           </a>
 
-          <p className="flex flex-row mb-6">
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <p className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3">
               C#<b className="font-general-sans px-3">·</b>Spectre Console
             </p>
           </p>
-          <h3 className="link-hover text-4xl font-general-sans uppercase inline-block cursor-pointer">
-            I.C.Treats
-          </h3>
-          <p className="mt-3 text-3xl">Back-End Development</p>
+          <a href="https://github.com/Koyonari/Group03_PRG2Assignment">
+            <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
+              I.C.Treats
+            </h3>
+          </a>
+          <p className="mt-3 text-xl md:text-3xl">Back-End Development</p>
         </div>
 
         <div className="works-4 w-full">
@@ -106,18 +113,22 @@ function Works() {
             </div>
           </a>
 
-          <p className="flex flex-row mb-6">
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <p className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-2xl border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3">
               Java
             </p>
           </p>
-          <h3 className="link-hover text-4xl font-general-sans uppercase inline-block cursor-pointer">
-            PlanHub
-          </h3>
-          <p className="mt-3 text-3xl">Mobile Application Development</p>
+          <a href="https://github.com/RISHIKEsH12321/MAD24_P01_Team3">
+            <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
+              PlanHub
+            </h3>
+          </a>
+          <p className="mt-3 text-xl md:text-3xl">
+            Mobile Application Development
+          </p>
         </div>
       </div>
     </section>
