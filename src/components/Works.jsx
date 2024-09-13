b937da880eb36f2285534266192025884b96cfb9
An Yong Shyan <yongshyan.an@gmail.com>
2024-09-14 04:07:58 +0800

    works section responsiveness

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,4 @@
-// Works section
-// Import images
+import React from "react";
 import ictreats from "../images/works/ictreats.png";
 import portfolio from "../images/works/portfolio.png";
 import nutriaid from "../images/works/nutriaid.png";
@@ -8,33 +7,33 @@ import planhub from "../images/works/planhub.png";
 function Works() {
   return (
     <section className="works lg:m-12" id="works">
-      <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-general-sans mb-16 w-screen">
+      <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-general-sans mb-4 md:mb-12 w-screen">
         Recent Works.
       </h2>
 
-      <div className="works-container w-full md:grid gap-16 place-items-center grid-cols-[repeat(auto-fit,_minmax(600px,_1fr))]">
-        <div className="works-1">
+      <div className="works-container w-full grid gap-16 place-items-center grid-cols-1 lg:grid-cols-2">
+        <div className="works-1 w-full">
           <a href="https://nutriaid-ays.vercel.app/">
             <div className="works-box1 flex justify-center items-center rounded-[20px] cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg mb-4">
               <img
                 src={nutriaid}
                 loading="lazy"
-                className="works-img w-4/5 my-24 mx-12 rounded-2xl"
+                className="works-img w-4/5 my-7 sm:my-24 mx-4 sm:mx-12 rounded-2xl"
                 alt="NutriAID"
               />
             </div>
           </a>
 
-          <p className="flex flex-col sm:flex-row mb-6">
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <div className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 overflow-x-auto">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 custom-scrollbar">
               HTML<b className="font-general-sans px-3">·</b>CSS
               <b className="font-general-sans px-3">·</b>Javascript
               <b className="font-general-sans px-3">·</b>Node.js
             </p>
-          </p>
+          </div>
           <a href="https://nutriaid-ays.vercel.app/">
             <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
               NutriAID
@@ -43,28 +42,28 @@ function Works() {
 
           <p className="mt-3 text-xl md:text-3xl">Back-End Development</p>
         </div>
-        <div className="works-2">
+        <div className="works-2 w-full">
           <a href="https://github.com/Koyonari/ays-portfolio">
             <div className="works-box2 flex justify-center items-center rounded-[20px] cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg mb-4">
               <img
                 src={portfolio}
                 loading="lazy"
-                className="works-img w-4/5 my-24 mx-12 rounded-2xl"
+                className="works-img w-4/5 my-7 sm:my-24 mx-4 sm:mx-12 rounded-2xl"
                 alt="Portfolio"
               />
             </div>
           </a>
 
-          <p className="flex flex-col sm:flex-row mb-6">
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <div className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 z-10 custom-scrollbar">
               HTML<b className="font-general-sans px-3">·</b>Javascript
               <b className="font-general-sans px-3">·</b>TailwindCSS
               <b className="font-general-sans px-3">·</b>React.js
             </p>
-          </p>
+          </div>
           <a href="https://github.com/Koyonari/ays-portfolio">
             <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
               Portfolio
@@ -79,20 +78,20 @@ function Works() {
               <img
                 src={ictreats}
                 loading="lazy"
-                className="works-img w-4/5 my-24 mx-12 rounded-2xl"
+                className="works-img w-4/5 my-7 sm:my-24 mx-4 sm:mx-12 rounded-2xl"
                 alt="I.C.Treats"
               />
             </div>
           </a>
 
-          <p className="flex flex-col sm:flex-row mb-6">
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <div className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 custom-scrollbar">
               C#<b className="font-general-sans px-3">·</b>Spectre Console
             </p>
-          </p>
+          </div>
           <a href="https://github.com/Koyonari/Group03_PRG2Assignment">
             <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
               I.C.Treats
@@ -107,20 +106,20 @@ function Works() {
               <img
                 src={planhub}
                 loading="lazy"
-                className="works-img w-4/5 my-24 mx-12 rounded-2xl"
+                className="works-img w-4/5 my-7 sm:my-24 mx-4 sm:mx-12 rounded-2xl"
                 alt="PlanHub"
               />
             </div>
           </a>
 
-          <p className="flex flex-col sm:flex-row mb-6">
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
+          <div className="flex flex-col sm:flex-row mb-6">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 mr-4">
               2024
             </p>
-            <p className="mt-3 text-sm sm:text-xl md:text-2xl overflow-x-auto border-stone-700 border-[4px] rounded-full p-3">
+            <p className="mt-3 text-sm sm:text-xl md:text-2xl lg:text-xl xl:text-2xl border-stone-700 border-[4px] rounded-full p-3 custom-scrollbar">
               Java
             </p>
-          </p>
+          </div>
           <a href="https://github.com/RISHIKEsH12321/MAD24_P01_Team3">
             <h3 className="link-hover text-2xl md:text-4xl font-general-sans uppercase inline-block cursor-pointer">
               PlanHub
